baebef728ccd63b9dea4c30c8ebabe81e7943b50
Andrew Mason <mason@case.edu>
2015-04-30 19:53:52 -0400

    adding solution for first session.

--- session1/gpa_calc.rb
+++ session1/gpa_calc.rb
@@ -1,7 +1,24 @@
 require 'csv'
 
 class Course
-  # TODO
+  attr_reader :credits
+
+  GRADE_TABLE = {
+    'A' => 4,
+    'B' => 3,
+    'C' => 2,
+    'D' => 1,
+    'F' => 0
+  }
+
+  def initialize(row)
+    @credits = row['Credits'].to_i
+    @grade = row['Grade']
+  end
+
+  def grade_points
+    GRADE_TABLE[@grade] * @credits
+  end
 end
 
 # ARGV contains the command line arguments. This basically "String[] args" from
@@ -15,12 +32,14 @@ file = ARGV[0]
 
 courses = []
 CSV.read(file, headers: true).each do |row|
-  # TODO
+  # '<<' is an alias for the 'push' method; they do the same thing.
+  courses << Course.new(row)
 end
 
 grade_points, credits = 0, 0
 courses.each do |course|
-  # TODO
+  grade_points += course.grade_points
+  credits += course.credits
 end
 
 gpa = grade_points.to_f / credits
